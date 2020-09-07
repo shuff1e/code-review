fc38d6bca5fbb982384d0cb3aa76049947de3161
徐霜馥 <xushuangfu@qiyi.com>
2020-09-07 16:56:22 +0800

    fix bug

--- Golang/main/main.go
+++ Golang/main/main.go
@@ -1,106 +1,25 @@
 package main
 
+import (
+	"github.com/emirpasic/gods/maps/linkedhashmap"
+	"github.com/emirpasic/gods/sets/hashset"
+	"github.com/robfig/cron"
+	"log"
+)
+
 func main() {
-	//arr := []int{1,2,3,4,5,6,7}
-	//Golang.DaisyChain(arr)
-	//ch := make(chan int,100)
-	//go Golang.Producer(ch)
-	//go Golang.Consumer(ch)
-	//done := make(chan struct{})
-	//Golang.Test2()
-	//<-done
-	//p := Golang.NewPublisher(100*time.Millisecond,10)
-	//docker := p.SubscribeTopic(func(v interface{}) bool {
-	//	if key,ok := v.(string);ok {
-	//		if strings.HasPrefix(key,"docker") {
-	//			return true
-	//		}
-	//	}
-	//	return false
-	//})
-	//
-	//go p.Publish("docker 1234")
-	//
-	//time.Sleep(time.Second*1)
-	//
-	//go func() {
-	//	fmt.Println(<-docker)
-	//}()
-	//go Golang.FillToken()
-	//go func() {
-	//	for {
-	//		result := Golang.TakeAvailable(false)
-	//		fmt.Println(result)
-	//		time.Sleep(time.Second)
-	//	}
-	//}()
-	//threadNum := 5
-	//for i := 0;i<threadNum;i++ {
-	//	Golang.Wg.Add(1)
-	//	go Golang.IncCounter(i)
-	//}
-	//Golang.Wg.Wait()
-	//fmt.Println(Golang.Counter)
-	//Golang.Test5()
-	//time.Sleep(time.Second*3)
-	//var wg sync.WaitGroup
-	//for i := 0;i<10;i++ {
-	//	wg.Add(1)
-	//	go func() {
-	//		defer wg.Done()
-	//		Golang.Incr()
-	//	}()
-	//}
-	//wg.Wait()
-	//done := make(chan struct{})
-	//Golang.Test6()
-	//indexes := make([]int,10)
-	//for i := 0;i<len(indexes);i++ {
-	//	indexes[i] = i
-	//}
-	//Golang.Shuffle(indexes)
-	//fmt.Printf("%#v\n",indexes)
-	//
-	//<- done
-	//	pool := Golang.NewPool(100)
-	//	println(runtime.NumGoroutine())
-	//	for i:=0;i<1000;i++ {
-	//		pool.Add(1)
-	//		go func() {
-	//			time.Sleep(time.Second)
-	//			println(runtime.NumGoroutine())
-	//			pool.Done()
-	//		}()
-	//	}
-	//	pool.Wait()
-	//	println(runtime.NumGoroutine())
-	//Golang.Test7()
-	//mutex := sync.Mutex{}
-	//mutex.Unlock()
-	//c := Golang.Boring("boring")
-	//c := Golang.FanIn(Golang.Boring("Joe"),Golang.Boring("Ann"))
-	//for i :=0;i<10;i++ {
-	//	msg1 := <-c;fmt.Println(msg1.Str)
-	//	msg2 := <-c;fmt.Println(msg2.Str)
-		//fmt.Printf("you sat %q\n", <-c)
-		//msg1.Wait <- true
-		//msg2.Wait <- true
-	//}
-	//fmt.Println("you are boring,i am leaving")
-	//
-	//ch := make(chan int)
-	//close(ch)
-	//select {
-	//case <- ch:
-	//	fmt.Println("fuck")
-	//}
-	//rand.Seed(time.Now().UnixNano())
-	//start := time.Now()
-	//results := Golang.Google("golang")
-	//elapsed := time.Since(start)
-	//fmt.Println(results)
-	//fmt.Println(elapsed)
-	//arr := []int{}
-	//arr = append(arr,0)
-	//fmt.Printf("%#v\n",arr)
+	m := linkedhashmap.New()
+	m.Put(1,2)
+
+	set := hashset.New()   // empty
+	set.Add(1)
+
+	c := cron.New()
+
+	c.AddFunc("@every 1s",func() {
+		log.Println("hello world")
+	})
+	c.Start()
+	select {
+	}
 }
---
+++ sword/1/singleton.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+// 实现一个单例类
+
+// sync.Once
+
+// atomic 提供的原子操作能够确保任一时刻只有一个goroutine对变量进行操作，
+// 善用 atomic 能够避免程序中出现大量的锁操作。
+
+// 载入操作能够保证原子的读变量的值，当读取的时候，任何其他CPU操作都无法对该变量进行读写，
+// 其实现机制受到底层硬件的支持。
+
+// 优先 channel
+// 其次 atomic
+// 其次 mutex，cond
+// https://www.bwangel.me/2019/04/13/go-sync-channel/
+
+type Once struct {
+	done uint32
+	mutex sync.Mutex
+}
+
+func (o *Once) Do(f func()) {
+	// Note: Here is an incorrect implementation of Do:
+	//
+	//	if atomic.CompareAndSwapUint32(&o.done, 0, 1) {
+	//		f()
+	//	}
+	//
+	// Do guarantees that when it returns, f has finished.
+	// This implementation would not implement that guarantee:
+	// given two simultaneous calls, the winner of the cas would
+	// call f, and the second would return immediately, without
+	// waiting for the first's call to f to complete.
+	// This is why the slow path falls back to a mutex, and why
+	// the atomic.StoreUint32 must be delayed until after f returns.
+
+	// https://stackoverflow.com/questions/56423412/sync-once-implementation
+
+	if atomic.LoadUint32(&o.done) == 0 {
+		o.doSlow(f)
+	}
+}
+
+func (o *Once) doSlow(f func()) {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+	if o.done == 0 {
+		//defer func() { o.done = 1}()
+		// StoreUint32 is necessary because the field is read concurrently, without holding the lock, by LoadUint32, right there in the same function.
+		// https://stackoverflow.com/questions/55964014/why-sync-once-using-atomic-in-doslow
+		defer atomic.StoreUint32(&o.done,1)
+		f()
+	}
+}
+
+
+// test code here
+
+var a = 0
+func assign() {
+	a = a+1
+}
+
+func main() {
+	o := Once{}
+	wg := sync.WaitGroup{}
+
+	for i := 0;i<5;i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			o.Do(assign)
+		}()
+	}
+	wg.Wait()
+	fmt.Println(a)
+}

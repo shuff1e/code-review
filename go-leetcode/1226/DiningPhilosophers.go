32e3deaac8851cc20ae22456d5ab22d089b26310
徐霜馥 <xushuangfu@qiyi.com>
2020-10-23 00:07:19 +0800

    fix bug

--- go-leetcode/1226/DiningPhilosophers.go
+++ go-leetcode/1226/DiningPhilosophers.go
@@ -335,3 +335,79 @@ func main()  {
 
 
   */
+
+ /*
+
+
+ import (
+     "fmt"
+     "runtime"
+     "strconv"
+     "strings"
+     "sync"
+ )
+
+ type ReentrantLock struct {
+     mu *sync.Mutex
+     cond *sync.Cond
+     owner int
+     holdCount int
+ }
+
+ func NewReentrantLock() sync.Locker {
+     rl := &ReentrantLock{}
+     rl.mu = new(sync.Mutex)
+     rl.cond = sync.NewCond(rl.mu)
+     return rl
+ }
+
+ func GetGoroutineId() int {
+     defer func()  {
+         if err := recover(); err != nil {
+             fmt.Println("panic recover:panic info:%v", err)     }
+     }()
+
+     var buf [64]byte
+     n := runtime.Stack(buf[:], false)
+     idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
+     id, err := strconv.Atoi(idField)
+     if err != nil {
+         panic(fmt.Sprintf("cannot get goroutine id: %v", err))
+     }
+     return id
+ }
+
+ func (rl *ReentrantLock) Lock() {
+     me := GetGoroutineId()
+     rl.mu.Lock()
+     defer rl.mu.Unlock()
+
+     if rl.owner == me {
+         rl.holdCount++
+         return
+     }
+     for rl.holdCount != 0 {
+         rl.cond.Wait()
+     }
+     rl.owner = me
+     rl.holdCount = 1
+ }
+
+ func (rl *ReentrantLock) Unlock() {
+     rl.mu.Lock()
+     defer rl.mu.Unlock()
+
+     if rl.holdCount == 0 || rl.owner != GetGoroutineId() {
+         panic("illegalMonitorStateError")
+     }
+     rl.holdCount--
+     if rl.holdCount == 0 {
+         rl.cond.Signal()
+     }
+ }
+
+
+  */
+
+ // https://studygolang.com/topics/6139/comment/18192
+ // https://stackoverflow.com/questions/14670979/recursive-locking-in-go#14671462

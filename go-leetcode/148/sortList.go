56957743938aa3dea17ea40136746f664beac36e
徐霜馥 <xushuangfu@qiyi.com>
2020-11-21 23:20:19 +0800

    fix bug

--- go-leetcode/148/sortList.go
+++ go-leetcode/148/sortList.go
@@ -197,3 +197,87 @@ func help(head *ListNode) (*ListNode,*ListNode) {
 	tail1.Next = head2
 	return head1,tail2
 }
+
+/*
+
+循环的归并排序
+
+方法二：自底向上归并排序
+使用自底向上的方法实现归并排序，则可以达到 O(1) 的空间复杂度。
+
+首先求得链表的长度 length，然后将链表拆分成子链表进行合并。
+
+具体做法如下。
+
+用 subLength 表示每次需要排序的子链表的长度，初始时 subLength=1。
+
+每次将链表拆分成若干个长度为 subLength 的子链表（最后一个子链表的长度可以小于 subLength），
+按照每两个子链表一组进行合并，合并后即可得到若干个长度为 subLength×2 的有序子链表（最后一个子链表的长度可以小于 subLength×2）。
+合并两个子链表仍然使用「21. 合并两个有序链表」的做法。
+
+将 subLength 的值加倍，重复第 2 步，对更长的有序子链表进行合并操作，直到有序子链表的长度大于或等于 length，整个链表排序完毕。
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        int length = 0;
+        ListNode node = head;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        ListNode dummyHead = new ListNode(0, head);
+        for (int subLength = 1; subLength < length; subLength <<= 1) {
+            ListNode prev = dummyHead, curr = dummyHead.next;
+            while (curr != null) {
+                ListNode head1 = curr;
+                for (int i = 1; i < subLength && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode head2 = curr.next;
+                curr.next = null;
+                curr = head2;
+                for (int i = 1; i < subLength && curr != null && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode next = null;
+                if (curr != null) {
+                    next = curr.next;
+                    curr.next = null;
+                }
+                ListNode merged = merge(head1, head2);
+                prev.next = merged;
+                while (prev.next != null) {
+                    prev = prev.next;
+                }
+                curr = next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+}
+
+ */
--- zzz/zzz.go
+++ zzz/zzz.go
@@ -3,21 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{1,1,2,2,9,13,15}
-	fmt.Println(lowBound(arr,5))
+	arr := []int{1,1,2,2,3,5,6}
+	k := 4
+	result := help(arr,k)
+	// 2
+	fmt.Println(result)
 }
 
-func lowBound(arr []int,x int ) int {
-	left := 0
-	right := len(arr)-1
-	for left <= right {
-		mid := (left + right)/2
-		if x < arr[mid] {
-			right = mid -1
+func help(arr []int,k int) int {
+	l := 0
+	r := len(arr) - 1
+	for l < r {
+		mid := (l + r)/2
+		if arr[mid] < k {
+			l = mid + 1
 		} else {
-			left = mid + 1
+			r = mid
 		}
 	}
-	return right
+	return l
 }
-

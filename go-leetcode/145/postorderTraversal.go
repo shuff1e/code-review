4a053b03c2494a443a5896039a200c9504cf3fce
shuffle <sfxu@foxmail.com>
2021-11-25 14:42:15 +0800

    Update postorderTraversal.go

--- go-leetcode/145/postorderTraversal.go
+++ go-leetcode/145/postorderTraversal.go
@@ -53,3 +53,29 @@ func postorderTraversal(root *TreeNode) []int {
 	}
 	return result
 }
+
+/*
+func postorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return nil
+    }
+    result := make([]int,0)
+    stack := make([]*TreeNode,0)
+    stack = append(stack,root)
+
+    for len(stack) > 0 {
+        temp := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+
+        result = append([]int{temp.Val},result...)
+        
+        if temp.Left != nil {
+            stack = append(stack,temp.Left)
+        }
+        if temp.Right != nil {
+            stack = append(stack,temp.Right)
+        }
+    }
+    return result
+}
+*/

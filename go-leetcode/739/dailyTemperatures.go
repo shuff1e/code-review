053573979dcd536be0c51694274f34eaa0ea19af
shuffle <shuffle@192.168.1.14>
2026-04-25 21:22:40 +0800

    Add missing Hot 100 problem stubs

---
+++ go-leetcode/739/dailyTemperatures.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+739. 每日温度
+
+给定一个整数数组 temperatures，其中 temperatures[i] 表示第 i 天的气温。
+请返回一个数组 answer，answer[i] 表示第 i 天之后需要等待多少天才会出现更高的气温。
+如果之后不会再出现更高的气温，则 answer[i] 为 0。
+
+示例 1：
+输入：temperatures = [73,74,75,71,69,72,76,73]
+输出：[1,1,4,2,1,1,0,0]
+
+示例 2：
+输入：temperatures = [30,40,50,60]
+输出：[1,1,1,0]
+
+示例 3：
+输入：temperatures = [30,60,90]
+输出：[1,1,0]
+
+提示：
+1 <= temperatures.length <= 10^5
+30 <= temperatures[i] <= 100
+*/
+
+func dailyTemperatures(temperatures []int) []int {
+	return nil
+}
---
+++ go-leetcode/763/partitionLabels.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+763. 划分字母区间
+
+给定一个字符串 s，将它划分为尽可能多的片段，使得同一个字母最多只出现在一个片段中。
+返回每个片段的长度。
+
+划分后的片段按原顺序连接后，应当仍然等于原字符串。
+
+示例 1：
+输入：s = "ababcbacadefegdehijhklij"
+输出：[9,7,8]
+解释：划分结果为 "ababcbaca"、"defegde"、"hijhklij"。
+
+示例 2：
+输入：s = "eccbbbbdec"
+输出：[10]
+
+提示：
+1 <= s.length <= 500
+s 仅包含小写英文字母。
+*/
+
+func partitionLabels(s string) []int {
+	return nil
+}

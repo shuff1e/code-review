9dea84b6bf7568ae2bb589788077ee4e147c95c2
shuffle <shuffle@192.168.1.14>
2026-04-26 11:19:01 +0800

    dailyTemperatures

--- go-leetcode/739/dailyTemperatures.go
+++ go-leetcode/739/dailyTemperatures.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 739. 每日温度
 
@@ -24,6 +26,44 @@ package main
 30 <= temperatures[i] <= 100
 */
 
+// 单调栈
+// 单调递减
+
+func main() {
+	arr := []int{73, 74, 75, 71, 69, 72, 76, 73}
+	result := dailyTemperatures(arr)
+	fmt.Println(result)
+
+	arr = []int{30, 40, 50, 60}
+	result = dailyTemperatures(arr)
+	fmt.Println(result)
+
+	arr = []int{30, 30, 30, 60}
+	result = dailyTemperatures(arr)
+	fmt.Println(result)
+}
+
 func dailyTemperatures(temperatures []int) []int {
-	return nil
+	if len(temperatures) == 0 {
+		return []int{}
+	}
+
+	result := make([]int, len(temperatures))
+
+	stack := []int{}
+
+	for i := 0; i < len(temperatures); i++ {
+		for len(stack) > 0 {
+			peek := stack[len(stack)-1]
+			if temperatures[peek] >= temperatures[i] {
+				break
+			}
+			stack = stack[:len(stack)-1]
+
+			result[peek] = i - peek
+		}
+		stack = append(stack, i)
+	}
+
+	return result
 }
